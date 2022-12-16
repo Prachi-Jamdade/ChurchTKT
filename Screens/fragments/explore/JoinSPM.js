c6da95d9927d1047fe058ea60fe02df9c9bc2ccd
Prachi-Jamdade <prachi.jamdade@codemonk.in>
2022-12-16 22:40:47 +0530

    added style to join spm form

--- Screens/fragments/explore/JoinSPM.js
+++ Screens/fragments/explore/JoinSPM.js
@@ -1,41 +1,50 @@
 import React from "react";
-import { View,Image,Dimensions,ScrollView, Text, TextInput, TouchableHighlight,TouchableOpacity } from "react-native";
+import { View, Image, Dimensions, ScrollView, Text, TextInput, TouchableHighlight, TouchableOpacity } from "react-native";
 import { StyleSheet } from "react-native";
 import gobalStyle from '../../styles/index';
 import { AppContext } from '../../../context';
 import { useState, useContext, useEffect } from "react";
 import ImageBackUp from '../../assests/icons/back.png';
-import {sendSPMFrom} from '../../api/requestForms'
-const JoinSPM = ({navigation}) => {
-    const {user}=useContext(AppContext);
- 
+import { sendSPMFrom } from '../../api/requestForms'
+import RadioButtonRN from 'radio-buttons-react-native';
+
+const JoinSPM = ({ navigation }) => {
+    const { user } = useContext(AppContext);
+
     const [data, setData] = useState({
         userName: '',
         fatherName: '',
-        "userId":user.userId,
-        "gender":"",
-        "amount":0
+        "userId": user.userId,
+        "gender": "",
+        "amount": 0
     });
 
 
- 
+    const genderData = [
+        {
+            label: 'Male'
+        },
+        {
+            label: 'Female'
+        }
+    ];
 
-    const change = (name,value) => {
-        setData({...data,[name]: value});
+    const change = (name, value) => {
+        setData({ ...data, [name]: value });
     };
     const submit = () => {
         console.log(data);
-        sendSPMFrom({...data}).then(()=>{
+        sendSPMFrom({ ...data }).then(() => {
             alert("Join the spm")
             setData({
                 userName: '',
                 fatherName: '',
-                userId:user.userId,
-                gender:"",
-                amounts:0
+                userId: user.userId,
+                gender: "",
+                amounts: 0
             })
             navigation.navigate("Spm")
-        }).catch((e)=>{
+        }).catch((e) => {
             alert("Something went wrong")
         })
     };
@@ -44,89 +53,104 @@ const JoinSPM = ({navigation}) => {
     return (
         <View style={gobalStyle.main}>
             <TouchableOpacity
-            style={gobalStyle.nav}
+                style={gobalStyle.nav}
                 // provide navigate path
-                    onPress={() => navigation.navigate('Explore')}
-                >
+                onPress={() => navigation.navigate('Explore')}
+            >
                 <Image source={ImageBackUp} style={gobalStyle.nav_image} />
                 <Text style={gobalStyle.nav_header}>Join SPM</Text>
             </TouchableOpacity>
 
-            <View style= {[styles.box]}>
- 
+            <View style={[styles.box]}>
 
-            <Text style={styles.boldText}>
-                We need some details
-            </Text>
 
-            <Text style={styles.lightText}>
-                Please provide us some of your information to be a part of SPM
-            </Text>
-            <ScrollView
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}>
+                <Text style={styles.boldText}>
+                    We need some details
+                </Text>
 
-            <TextInput style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Your Name"
-                placeholderTextColor="#808080"
-                autoCapitalize="none"
-                name="userName"
-                value={data.userName}
-                onChangeText={(e)=>change("userName",e)}
-            />
+                <Text style={styles.lightText}>
+                    Please provide us some of your information to be a part of SPM
+                </Text>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}>
 
-            <TextInput style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Father's Name"
-                placeholderTextColor="#808080"
-                autoCapitalize="none"
-                name="fatherName"
-                value={data.fatherName}
-                onChangeText={(e)=>change("fatherName",e)}
-            />
+                    <TextInput style={styles.input}
+                        underlineColorAndroid="transparent"
+                        placeholder="Your Name"
+                        placeholderTextColor="#808080"
+                        autoCapitalize="none"
+                        name="userName"
+                        value={data.userName}
+                        onChangeText={(e) => change("userName", e)}
+                    />
 
-            <TextInput style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Gender"
-                placeholderTextColor="#808080"
-                autoCapitalize="none"
-                name="gender"
-                value={data.gender}
-                onChangeText={(e)=>change("gender",e)}
-            />
+                    <TextInput style={styles.input}
+                        underlineColorAndroid="transparent"
+                        placeholder="Father's Name"
+                        placeholderTextColor="#808080"
+                        autoCapitalize="none"
+                        name="fatherName"
+                        value={data.fatherName}
+                        onChangeText={(e) => change("fatherName", e)}
+                    />
 
-            {/* <Text style={{fontFamily: 'Montserrat-Medium', fontSize: 16, margin:10, color: 'white'}}>Gender</Text> */}
-            <Text style={[styles.lightText,{marginTop:30}]}>Enter the amount you want to commit: </Text>
-           
-            <TextInput style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Amount (in INR)"
-                placeHolderTextColor="#989898"
-                keyboardType="number-pad"
-                placeholderTextColor="white"
-                autoCapitalize="none"
-                name="amount"
-                onChangeText={(e)=>change("amount",parseInt(e))}
-                />
+                    {/* // <TextInput style={styles.input}
+                    //     underlineColorAndroid="transparent"
+                    //     placeholder="Gender"
+                    //     placeholderTextColor="#808080"
+                    //     autoCapitalize="none"
+                    //     name="gender"
+                    //     value={data.gender}
+                    //     onChangeText={(e) => change("gender", e)}
+                    // /> */}
+
+<Text style={[styles.lightText, marginBottom= 3]}>
+                    Gender
+                </Text>
+
+
+                    <RadioButtonRN
+                    data={genderData}
+                    selectedBtn={(e) => console.log(e)}
+                    style={{marginStart: 5}}
+                    activeColor={'#CF820E'}
+                    inactiveColor={'#181818'}
+                    boxStyle={{backgroundColor: '#1E1E1E', borderColor: '#1E1E1E'}}
+                    textStyle={{fontFamily: 'Montserrat-Medium', fontSize: 16, color: 'white'}}
+                    />
+
+                    {/* <Text style={{fontFamily: 'Montserrat-Medium', fontSize: 16, margin:10, color: 'white'}}>Gender</Text> */}
+                    <Text style={[styles.lightText, { marginTop: 30 }]}>Enter the amount you want to commit: </Text>
+
+                    <TextInput style={styles.input}
+                        underlineColorAndroid="transparent"
+                        placeholder="Amount (in INR)"
+                        placeHolderTextColor="#989898"
+                        keyboardType="number-pad"
+                        placeholderTextColor="white"
+                        autoCapitalize="none"
+                        name="amount"
+                        onChangeText={(e) => change("amount", parseInt(e))}
+                    />
                 </ScrollView>
 
-            <TouchableHighlight
-                style={gobalStyle.btn_abs}
-                onPress={() => { submit(); }}
-            >
-                <Text style={[gobalStyle.submitText]}>JOIN</Text>
-            </TouchableHighlight>
+                <TouchableHighlight
+                    style={gobalStyle.btn_abs}
+                    onPress={() => { submit(); }}
+                >
+                    <Text style={[gobalStyle.submitText]}>JOIN</Text>
+                </TouchableHighlight>
             </View>
         </View>
     )
 }
 
-const {width} = Dimensions.get('window');
-const boxWidth=width*0.9;
+const { width } = Dimensions.get('window');
+const boxWidth = width * 0.9;
 const styles = StyleSheet.create({
     input: {
-        height: 60,
+        height: 50,
         alignItems: 'center',
         justifyContent: 'center',
         marginVertical: 5,
@@ -142,30 +166,30 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontFamily: 'Montserrat-Regular'
     },
-    box : {
+    box: {
         width,
         backgroundColor: '#1E1E1E',
-        flexDirection:'column',
+        flexDirection: 'column',
         borderRadius: 20,
         flex: 1,
-        marginTop: 20,
-        paddingTop:20,
+        marginTop: 10,
+        paddingTop: 10,
     },
     boldText: {
         fontFamily: 'Montserrat-SemiBold',
         fontSize: 18,
-        marginStart: 10,
         color: 'white',
+        margin: 20,
     },
     lightText: {
         fontFamily: 'Montserrat-Medium',
         fontSize: 16,
-        margin: 10,
+        margin: 20,
         marginBottom: 10,
         color: '#989898',
     },
     normalText: {
-       
+
     }
 })
 
