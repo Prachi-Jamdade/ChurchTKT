b6dd7644c536e4166f5fc2bc398d48a36e67c580
Prachi-Jamdade <prachi.jamdade@codemonk.in>
2022-12-16 21:52:02 +0530

    added style to join spm

--- Screens/fragments/explore/JoinSPM.js
+++ Screens/fragments/explore/JoinSPM.js
@@ -1,81 +1,118 @@
 import React from "react";
-import { View, Text, TextInput, TouchableHighlight } from "react-native-animatable";
+import { View, Text, TextInput, TouchableHighlight, SafeAreaView } from "react-native";
 import { StyleSheet } from "react-native";
 import gobalStyle from '../../styles/index';
-import {AppContext} from '../../../context';
+import { AppContext } from '../../../context';
 import { useState, useContext, useEffect } from "react";
+import Icon, { Icons } from '../Icons';
+import RadioButtonRN from "radio-buttons-react-native";
 
 const JoinSPM = () => {
 
-    const [data, setData] = useState( {
+    const [data, setData] = useState({
         name: '',
         fatherName: '',
     });
 
-    const {user}=useContext(AppContext);
+    const { user } = useContext(AppContext);
 
-    useEffect(()=>{
-        setData({name:user.firstName+" "+user.lastName,phoneNo:user.phoneNumber})
-    },[user])
+    useEffect(() => {
+        setData({ name: user.firstName + " " + user.lastName, phoneNo: user.phoneNumber })
+    }, [user])
 
     const handleName = (text) => {
-        setData({...data,name: text});
+        // setData({ ...data, name: text });
     };
 
     const handleFatherName = (text) => {
-        setData({...data,fatherName: text});
+        // setData({ ...data, fatherName: text });
     };
 
-    const [amount,setAmount] = useState(0);
+    const [amount, setAmount] = useState(0);
+
+    const radioBtnData = [
+        {
+            label: 'Male'
+        },
+        {
+            label: 'Female'
+        }
+    ]
 
     return (
-        <View>
-            <Text style={boldText}>
-                We need some details
-            </Text>
+        <SafeAreaView style={gobalStyle.main}>
+            <View style={styles.container}>
+                <View style={gobalStyle.nav}>
+                    <View>
 
-            <Text style={lightText}>
-                Please provide us some of your information to be a part of SPM
-            </Text>
+                        <Icon
+                            style={{ paddingStart: 12 }}
+                            type={Icons.MaterialIcons}
+                            size={25}
+                            name="arrow-back-ios"
+                            color='white'
+                        />
+                    </View>
+                    <Text style={gobalStyle.nav_header}>Help</Text>
 
-            <TextInput style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Your Name"
-                placeholderTextColor="#808080"
-                autoCapitalize="none"
-                value={data.name}
-                onChangeText={handleName}
-            />
+                </View>
 
-            <TextInput style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Father's Name"
-                placeholderTextColor="#808080"
-                autoCapitalize="none"
-                value={data.name}
-                onChangeText={handleFatherName}
-            />
+                <View style={{ backgroundColor: '#1E1E1E', borderRadius: 20, flexDirection: 'column', width: '100%', height: '100%', padding: 20 }}>
+                    <Text style={styles.boldText}>
+                        We need some details
+                    </Text>
 
-            <Text style={{fontFamily: 'Montserrat-Medium', fontSize: 16, margin:10, color: 'white'}}>Gender</Text>
-            <Text style={{ fontFamily: 'Montserrat-Medium', color: 'white', fontSize: 18 }}>Enter the amount you want to commit: </Text>
-            <TextInput style={styles.input}
-                underlineColorAndroid="transparent"
-                placeholder="Amount (in INR)"
-                placeHolderTextColor="#989898"
-                keyboardType="number-pad"
-                placeholderTextColor="white"
-                autoCapitalize="none"
-                value={amount}
-                onChangeText={(text)=>{setAmount(text);}}
-            />
+                    <Text style={styles.lightText}>
+                        Please provide us some of your information to be a part of SPM
+                    </Text>
 
-            <TouchableHighlight
-                style={gobalStyle.btn_abs}
-                // onPress={() => { getOrder(); }}
-            >
-                <Text style={[gobalStyle.submitText]}>JOIN</Text>
-            </TouchableHighlight>
-        </View>
+                    <TextInput style={styles.input}
+                        underlineColorAndroid="transparent"
+                        placeholder="Your Name"
+                        placeholderTextColor="#808080"
+                        autoCapitalize="none"
+                        value={data.name}
+                        onChangeText={handleName}
+                    />
+
+                    <TextInput style={styles.input}
+                        underlineColorAndroid="transparent"
+                        placeholder="Father's Name"
+                        placeholderTextColor="#808080"
+                        autoCapitalize="none"
+                        value={data.name}
+                        onChangeText={handleFatherName}
+                    />
+
+                    <Text style={{ fontFamily: 'Montserrat-Medium', fontSize: 16, margin: 10, color: 'white' }}>Gender</Text>
+                    <RadioButtonRN data={radioBtnData}
+                    selectedBtn={(e) => console.log(e)}
+                    style={{backgroundColor: '#1E1E1E'}}
+                    boxStyle={{backgroundColor: '#1E1E1E'}} />
+                    <Text style={{ fontFamily: 'Montserrat-Medium', color: 'white', fontSize: 16, marginStart: 10, marginBottom: 10 }}>Enter the amount you want to commit: </Text>
+                    <TextInput style={styles.input}
+                        underlineColorAndroid="transparent"
+                        placeholder="Amount (in INR)"
+                        placeHolderTextColor="#989898"
+                        keyboardType="number-pad"
+                        placeholderTextColor="white"
+                        autoCapitalize="none"
+                        value={amount}
+                        onChangeText={(text) => { setAmount(text); }}
+                    />
+
+                    <TouchableHighlight
+                        style={gobalStyle.btn_abs}
+                    // onPress={() => { getOrder(); }}
+                    >
+                        <Text style={[gobalStyle.submitText]}>JOIN</Text>
+                    </TouchableHighlight>
+                </View>
+
+
+            </View>
+        </SafeAreaView>
+
     )
 }
 
@@ -86,34 +123,35 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginVertical: 5,
         marginHorizontal: 10,
-        marginStart: 20,
-        marginEnd: 20,
-        height: 40,
+        marginEnd: 10,
+        height: 50,
         borderColor: '#343739',
         borderWidth: 1,
         paddingHorizontal: 10,
         color: 'white',
+        borderRadius: 10,
+        borderWidth: 2,
         fontSize: 16,
         fontFamily: 'Montserrat-Regular'
     },
     boldText: {
         fontFamily: 'Montserrat-SemiBold',
-        fontSize: 14,
-        margin: 10,
+        fontSize: 18,
+        marginStart: 10,
         color: 'white',
     },
     lightText: {
         fontFamily: 'Montserrat-Regular',
-        fontSize: 15,
+        fontSize: 16,
         margin: 10,
         marginBottom: 10,
         color: '#989898'
     },
     normalText: {
-        
+
     }
 })
 
-export {styles};
+export { styles };
 
 export default JoinSPM;
